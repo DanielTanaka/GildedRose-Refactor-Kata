e208e4ae4456f467d61fa1a6102ccde9b1f761ed
Daniel Tanaka <danielh.f.tanaka@gmail.com>
2021-02-26 23:39:38 -0300

    Removed a comment

--- GildedRose.Service/QualityUpdaterStrategy/LegendaryItemQualityUpdater.cs
+++ GildedRose.Service/QualityUpdaterStrategy/LegendaryItemQualityUpdater.cs
@@ -7,7 +7,6 @@ namespace GildedRose.Service.QualityUpdaterStrategy
         public void UpdateItemQuality(IItem item)
         {
             //Do nothing. Legendary items do not need to be sold and never have their Quality decreased.
-            //Check if there's a better way to implement something.
         }
     }
 }
